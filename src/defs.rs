b170f69cada8e9ba9429ab6ee719f7dda9660c02
skrytt <matt.kemp.1992@gmail.com>
2018-11-17 16:12:50 +0000

    Use default block size

--- src/defs.rs
+++ src/defs.rs
@@ -5,7 +5,7 @@ pub type Volume = f32;
 pub type Output = f32;
 
 pub const CHANNELS: usize = 2;
-pub const FRAMES: u32 = 64;
+pub const FRAMES: u32 = 0;
 pub const SAMPLE_HZ: f64 = 44_100.0;
 pub const MIDI_BUF_LEN: usize = 1024;
 pub const PROGNAME: &str = "baselisk";
