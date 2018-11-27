1e8ea91555c667f50223fa4bcef89eff30c7395a
skrytt <matt.kemp.1992@gmail.com>
2018-11-27 23:26:08 +0000

    Reduce channels to 1 (monophonic)

--- src/defs.rs
+++ src/defs.rs
@@ -5,7 +5,7 @@ pub type Volume = f32;
 pub type Output = f32;
 pub type Frame = [Output; CHANNELS];
 
-pub const CHANNELS: usize = 2;
+pub const CHANNELS: usize = 1;
 pub const FRAMES: u32 = 0;
 pub const SAMPLE_HZ: f64 = 44_100.0;
 pub const MIDI_BUF_LEN: usize = 1024;
