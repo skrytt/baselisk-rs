f3c241e72076ff0525f65e6b5259b39d04bedba0
skrytt <matt.kemp.1992@gmail.com>
2019-03-31 18:53:54 +0100

    Placeholder for midi learn

--- src/processor/modmatrix.rs
+++ src/processor/modmatrix.rs
@@ -5,6 +5,7 @@ use event::{EngineEvent, MidiEvent, ModulatableParameter};
 pub struct ModulationMatrix
 {
     controllers: Vec<SingleController>,
+    parameter_to_learn: Option<ModulatableParameter>,
 }
 
 impl ModulationMatrix
@@ -12,7 +13,13 @@ impl ModulationMatrix
     pub fn new() -> ModulationMatrix {
         ModulationMatrix {
             controllers: vec![SingleController::new(); 128], // Space for CCs 0 through 127
+            parameter_to_learn: None,
         }
+    }
+
+    /// Set that a parameter should be bound to the next MIDI CC received.
+    pub fn _learn_parameter(&mut self, parameter: ModulatableParameter) {
+        self.parameter_to_learn = Some(parameter);
     }
 
     pub fn bind_cc(&mut self, number: u8, parameter: ModulatableParameter) {
@@ -22,10 +29,19 @@ impl ModulationMatrix
 
     /// Process a MidiEvent.
     /// Maybe emit an EngineEvent::ModulateParameter.
-    pub fn process_event(&self, event: &MidiEvent) -> Option<EngineEvent> {
+    pub fn process_event(&mut self, event: &MidiEvent) -> Option<EngineEvent> {
         if let MidiEvent::ControlChange { number, value } = event {
-            let controller = self.controllers.get(*number as usize).unwrap();
-            return controller.process(*value)
+            let is_midi_learn = self.parameter_to_learn.is_some();
+            match is_midi_learn {
+                false => {
+                    let controller = self.controllers.get(*number as usize).unwrap();
+                    return controller.process(*value)
+                },
+                true => {
+                    let mut controller = self.controllers.get_mut(*number as usize).unwrap();
+                    controller.bind(self.parameter_to_learn.take().unwrap());
+                }
+            }
         }
         None
     }
@@ -47,7 +63,7 @@ impl SingleController
         }
     }
 
-    /// Bind a CC number to a target Parameter.
+    /// Bind this controller to a ModulatableParameter.
     pub fn bind(&mut self, parameter: ModulatableParameter) {
         self.parameter = Some(parameter);
     }
